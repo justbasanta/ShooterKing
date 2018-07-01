63f593c88ba3e267a61c81991465062b1ce96fb6
Basanta Thapa <justbasanta5@gmail.com>
2018-07-01 13:00:40 +0545

    Frame animation completed

--- script.js
+++ script.js
@@ -8,8 +8,18 @@ var $button = document.getElementById('fly-button');
 
 $button.onclick = function(){
 	console.log('clicked');
+	var pos = 0;
 	$homescreen.style.display = 'none';
 	$background.style.backgroundImage = 'url("./images/clouds.jpg")';
 	this.style.display = 'none';
 
+	setInterval (function(){
+		if (pos == 600) {
+			pos = 0;
+		}
+		else{
+			pos++;
+			$background.style.backgroundPosition = "0px" + " " + pos +"px";	
+		}
+	},15)
 }
