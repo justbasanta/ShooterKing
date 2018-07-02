f42a4cbc6d870d4253ad5d4ad890b3a63bea8378
Basanta Thapa <justbasanta5@gmail.com>
2018-07-02 23:28:27 +0545

    Added Game over screen and fixed earlier bugs

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ var bullet_count=0;
 var bullets = [];
 var enemies_car = [];
 var invunerables_array = [];
-var game_status = true;
+var gamestatus = false;
+var score= 0;
+var run;
 
 var $main = document.getElementById('main');
 var $homescreen = document.getElementById('homescreen-wrapper');
 var $background = document.getElementById('background');
+var $scoreboard = document.getElementById('scoreboard');
 var $button = document.getElementById('fly-button');
 
 var frame = new Frame();
@@ -19,17 +22,21 @@ $button.onclick = function(){
 	console.log('clicked');
 	$homescreen.style.display = 'block';
 	$background.style.display = 'block';
+
 	this.style.display = 'none';
 	main();
 }
 function main(){
 	console.log('main started');
 	resetGame();
-	setInterval(MainGameLoop,15);
+	car.resetCarPosition();
+	gamestatus = true;
+	run = setInterval(MainGameLoop,30);
 }
 
 function MainGameLoop(){
 	// console.log('MainGameLoop Entered');
+	$scoreboard.style.display='block';
 	createEnemies();
 	updateEnemies();
 	updateBullets();
@@ -53,6 +60,10 @@ function Frame(){
 		self.background_dY = speed_dy;
 		$background.style.backgroundPosition = "0px " + self.backgroundY+ "px";
 		self.backgroundY = self.backgroundY + self.background_dY;
+		if (counter%2==0) {
+			score+=1;
+		}
+		$scoreboard.innerHTML = score;
 	}
 }
 
@@ -95,15 +106,15 @@ function Enemy(){
 	this.dy = 0;
 	this.health = 500;
 	var self = this;
-	var $new_enemy = document.createElement('div');
+	this.new_enemy = document.createElement('div');
 
 	this.createEnemy = function(){
 		// console.log(self.$new_enemy);
-		$new_enemy.className = 'enemy';
+		self.new_enemy.className = 'enemy';
 		self.x = self.getRandom();
-		$new_enemy.style.left = self.x + 'px';
-		$new_enemy.style.top = self.y + 'px';
-		$background.appendChild($new_enemy);
+		self.new_enemy.style.left = self.x + 'px';
+		self.new_enemy.style.top = self.y + 'px';
+		$background.appendChild(self.new_enemy);
 	}
 	this.getRandom = function(){
 		var random= Math.random();
@@ -124,14 +135,15 @@ function Enemy(){
 	this.updateEnemy = function(){
 		self.dy = speed_dy;
 		self.y = self.y + self.dy;
-		$new_enemy.style.top = self.y+'px';
+		self.new_enemy.style.top = self.y+'px';
 	}
 	this.deleteEnemy = function(){
-		$background.removeChild($new_enemy);
+		$background.removeChild(self.new_enemy);
 	}
 }
 
 function createEnemies(){
+	console.log("IN CREATE ENEMIES");
 	if(counter > 80-(speed_dy*3))
 		{	
 			var e = new Enemy();
@@ -141,22 +153,21 @@ function createEnemies(){
 		}
 }
 function updateEnemies(){
-	var enemies = enemies_car;
-	console.log(enemies);
-		for(var i=0;i<enemies.length;i++)
+	console.log("IN UPDATE ENEMIES");
+	console.log(enemies_car);
+		for(var i=0;i<enemies_car.length;i++)
 		{
+			if(enemies_car[i] !== null){
+				enemies_car[i].updateEnemy();
+				if(enemies_car[i].y > 500)
+				{
+					enemies_car[i].deleteEnemy();
+					enemies_car[i]=null;
+				}
+			}				
 			// console.log(enemies[i]);
-			enemies[i].updateEnemy();
-			// console.log(enemies[i]);
-			if(enemies[i].y > 600)
-			{
-				enemies[i].deleteEnemy();
-				enemies.splice(i, 1);
-				// enemies[i]=null;
-				// enemies_cars = clearArray(enemies);
-				break;
-			}
-	}
+		}
+		enemies_car = clearArray(enemies_car);
 }
 
 
@@ -166,47 +177,49 @@ function Bullet(){
 	this.dy = 10;
 
 	var self = this;
-	var $new_bullet = document.createElement('div');
+	this.new_bullet = document.createElement('div');
 
 	this.createBullet = function(){
 		// console.log($new_bullet);
-		$new_bullet.className = 'bullet';
+		self.new_bullet.className = 'bullet';
 		self.bulletX = car.carX + 45;
 		self.bulletY = car.carY;
-		$new_bullet.style.left = self.bulletX + 'px';
-		$new_bullet.style.top = self.bulletY + 'px';
-		$main.appendChild($new_bullet);
+		self.new_bullet.style.left = self.bulletX + 'px';
+		self.new_bullet.style.top = self.bulletY + 'px';
+		$main.appendChild(self.new_bullet);
 	}
 	this.updateBullet = function(){
 		var t = self.bulletY - self.dy;
 		self.bulletY = t;
-		$new_bullet.style.top = self.bulletY+"px";
+		self.new_bullet.style.top = self.bulletY+"px";
 	}
 	this.deleteBullet = function(){
-		$main.removeChild($new_bullet);
+		$main.removeChild(self.new_bullet);
 	}
 }
 
 function updateBullets(){
-	var temp = bullets;
+	console.log("IN UPDATE BULLETS");
 	for(var i=0; i<bullets.length; i++){
-		temp[i].updateBullet();
-		if (temp[i].bulletY < 0) {
-			temp[i].deleteBullet();
-			temp[i]=null;
-			bullets = clearArray(temp);
+		if(bullets[i] !== null){
+			bullets[i].updateBullet();
+			if (bullets[i].bulletY < 0) {
+				bullets[i].deleteBullet();
+				bullets[i] = null;
+				bullets = clearArray(bullets);
+			}
 		}
 	}
 }
 
 function resetGame(){
-	// var enemies = enemies_cars;
-	// 	for(var i =0; i<enemies.length;i++)
-	// 		{
-	// 			enemies[i].deleteEnemy();
-	// 			enemies[i] =null;
-	// 		}
-	// 	enemies_cars = clearArray(enemies);
+	var enemies = enemies_car;
+		for(var i =0; i<enemies.length;i++)
+			{
+				enemies[i].deleteEnemy();
+				enemies[i] =null;
+			}
+		enemies_car = clearArray(enemies);
 		
 		var bullets_temp = bullets;
 		for(var i =0; i<bullets_temp.length;i++)
@@ -216,21 +229,22 @@ function resetGame(){
 			}
 		bullets = clearArray(bullets_temp);
 		
-		gamestatus="true";
-	// 	Frame.background_y =0;
+		gamestatus= true;
+		Frame.backgroundY =0;
 		counter=0; bullet_count=0;
-		// speedcounter=0;  invulcounter=0;
+		speedcounter=0;  invulcounter=0;
 		speed_dy=1;
-	// 	score=0;
+		score=0;
 		
-	// 	var gameover_background = document.getElementById("gameover-background");
-	// 	gameover_background.style.display = "none"
+		var gameover_background = document.getElementById("gameover");
+		gameover_background.style.display = "none";
+		$scoreboard.style.display = 'none';
 }
 
 function collisionCheck()
 	{
 		var carY = car.carY;
-		var carX = car.carX;
+		var carX = car.carX;	
 		var ex; var ey;
 		var bx; var by;
 		
@@ -244,6 +258,7 @@ function collisionCheck()
 		
 		for (var i=0;i<el;i++)
 			{
+				if(etemp[i] !== null && etemp[i] !== undefined){
 				ex = etemp[i].x;
 				ey = etemp[i].y;
 			  if ( 	(ex+90) > carX		//right edge of e > left edge of car
@@ -252,11 +267,13 @@ function collisionCheck()
 					  && ey<= (carY+90)	//top edge of e > bottom edge of car
 					)
 				{
-					// explodeAnim(carX,carY);
-					// var feedbackgameover = document.getElementById("feedbackgameover");
-					// feedbackgameover.innerHTML ="Game Over";
-				  // gameOver();
-				  // game_status = "false";
+					explodeAnim(carX,carY);
+					// alert('Game Over!!!!');
+					
+					var feedbackgameover = document.getElementById("gameovermsg");
+					feedbackgameover.innerHTML ="Game Over";
+					 gameOver();
+					 // gamestatus = false;
 				}
 				
 		// 		//Collision check for bullets
@@ -281,7 +298,7 @@ function collisionCheck()
 										if(etemp[i].health<=0)
 										{
 											explodeAnim(ex,ey);
-											enemies_car.splice(i, 1);
+											// enemies_car.splice(i, 1);
 											etemp[i].deleteEnemy();
 											etemp[i] = null;
 											score = score+50;
@@ -289,6 +306,7 @@ function collisionCheck()
 									 
 								}
 						}
+					}
 					}//for j end
 			}//for i end
 			
@@ -296,11 +314,12 @@ function collisionCheck()
 			enemies_car = clearArray(etemp);	
 	}
 
-	function hitAnim(ex,ey)
+function hitAnim(ex,ey)
 {
 	var hit_id = document.createElement('div');
  	hit_id.className = "explosion"; //style
-  	hit_id.style.left = (ex-10)+"px";	hit_id.style.top = (ey)+"px";
+  	hit_id.style.left = (ex-10)+"px";
+  	hit_id.style.top = (ey)+"px";
   	$main.appendChild(hit_id);
 	hit_id.style.backgroundPosition = "0px "+"0px";
 	
@@ -354,28 +373,30 @@ function explodeAnim(ex,ey)
 		$main.removeChild(explode_id);
 		clearInterval(animate);
 		}
-		console.log(animcounter);
+		// console.log(animcounter);
 		animcounter++;
 	}
 }
+function gameOver()
+	{
+		clearInterval(run);
+		var gameover_background = document.getElementById("gameover");
+		gameover_background.style.display = "block";
 
+		var score_display = document.getElementById("display-score");
+		score_display.innerHTML = score;
+	}
 
 
 function clearArray(input){
-	var temp = [];
-	var len = input.length;
-	for(var i=0; i<len; i++){
-		if (input[i]!= null) {
-			temp.push(input[i]);
-		}
-	}
-	return temp;
+	return input.filter(function(e){ return e !== null})
 }
 
 function keydownEventHandler(e)
 	{
+		// var homescreen = document.getElementById("homescreen-wrapper");
 		console.log(e.keyCode);
-		if(game_status)
+		if(gamestatus)
 			{
 				if(e.keyCode == 37)
 				{
@@ -395,7 +416,7 @@ function keydownEventHandler(e)
 					//left
 					if(bullet_count >= 5)
 					{
-						console.log('bullet created');
+						// console.log('bullet created');
 						var e = new Bullet();
 						(bullets).push(e);
 						e.createBullet();
@@ -405,18 +426,18 @@ function keydownEventHandler(e)
 				
 				if(e.keyCode == 27)
 				{
-					gameOver();
+					console.log("ESC PRESSED!!!");
 					resetGame();
-					var homescreen = document.getElementById("homescreen");
-					homescreen.style.display="block";
+					$homescreen.style.display="block";
+					$background.style.display="none";
+					$button.style.display="block";
 				}
 			}
 			else
 			{
 				if(e.keyCode == 27)
 				{
-					var homescreen = document.getElementById("homescreen");
-					homescreen.style.display="block";
+					$homescreen.style.display="block";
 				}
 			}
 	}
